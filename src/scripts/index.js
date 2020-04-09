1a0070caca4fa9f16d0705991ee961f11be3d3dd
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2020-04-09 22:38:09 +0200

    add parcel and prettier

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-const explorer = document.querySelector("#explorer ul");
+const explorer = document.querySelector("#explorer ul")
 
 const root = {
   id: 0,
@@ -6,37 +6,61 @@ const root = {
   name: "Home",
   children: [
     {
-      id: 1, name: "Amine", type: "folder", children: [
+      id: 1,
+      name: "Amine",
+      type: "folder",
+      children: [
         {
-          id: 5, name: "Amine ATJ", type: "folder", children: []
+          id: 5,
+          name: "Amine ATJ",
+          type: "folder",
+          children: [],
         },
         {
-          id: 6, name: "Amine Maria", type: "folder", children: []
+          id: 6,
+          name: "Amine Maria",
+          type: "folder",
+          children: [],
         },
-      ]
+      ],
     },
     {
-      id: 2, name: "ATJ", type: "folder", children: []
+      id: 2,
+      name: "ATJ",
+      type: "folder",
+      children: [],
     },
     {
-      id: 3, name: "Maria", type: "folder", children: []
+      id: 3,
+      name: "Maria",
+      type: "folder",
+      children: [],
     },
     {
-      id: 4, name: "Yasmine", type: "folder", children: [
+      id: 4,
+      name: "Yasmine",
+      type: "folder",
+      children: [
         {
-          id: 7, name: "Yasmine ATJ", type: "folder", children: []
+          id: 7,
+          name: "Yasmine ATJ",
+          type: "folder",
+          children: [],
         },
         {
-          id: 8, name: "Yasmine Maria", type: "folder", children: []
+          id: 8,
+          name: "Yasmine Maria",
+          type: "folder",
+          children: [],
         },
-      ]
+      ],
     },
-  ]
-};
+  ],
+}
 
-let currentFolder = root;
+let currentFolder = root
 
-main();
+main()
 
 /////
 function main() {
@@ -45,15 +69,18 @@ function main() {
 
 function renderExplorer() {
   explorer.innerHTML = currentFolder.children.reduce((accumulator, file) => {
-    return accumulator + `<li data-id="${file.id}" class="node"><span>${file.name}</span></li>`
-  }, "");
-  Array.from(explorer.querySelectorAll(".node")).forEach(
-    node => node.addEventListener("dblclick", respondToNodeDblClick)
+    return (
+      accumulator +
+      `<li data-id="${file.id}" class="node"><span>${file.name}</span></li>`
+    )
+  }, "")
+  Array.from(explorer.querySelectorAll(".node")).forEach((node) =>
+    node.addEventListener("dblclick", respondToNodeDblClick)
   )
 }
 
 function respondToNodeDblClick(e) {
-  const nextId = Number(e.currentTarget.dataset.id);
-  currentFolder = currentFolder.children.find(child => child.id === nextId)
+  const nextId = Number(e.currentTarget.dataset.id)
+  currentFolder = currentFolder.children.find((child) => child.id === nextId)
   renderExplorer()
 }
