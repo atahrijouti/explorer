b31905fbf2e31431d769f2d3ce6cfe04b4495ca5
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2020-04-09 23:03:31 +0200

    switch to a relational db approach

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,64 +1,25 @@
 const explorer = document.querySelector("#explorer ul")
 
-const root = {
-  id: 0,
-  type: "folder",
-  name: "Home",
-  children: [
-    {
-      id: 1,
-      name: "Amine",
-      type: "folder",
-      children: [
-        {
-          id: 5,
-          name: "Amine ATJ",
-          type: "folder",
-          children: [],
-        },
-        {
-          id: 6,
-          name: "Amine Maria",
-          type: "folder",
-          children: [],
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: "ATJ",
-      type: "folder",
-      children: [],
-    },
-    {
-      id: 3,
-      name: "Maria",
-      type: "folder",
-      children: [],
-    },
-    {
-      id: 4,
-      name: "Yasmine",
-      type: "folder",
-      children: [
-        {
-          id: 7,
-          name: "Yasmine ATJ",
-          type: "folder",
-          children: [],
-        },
-        {
-          id: 8,
-          name: "Yasmine Maria",
-          type: "folder",
-          children: [],
-        },
-      ],
-    },
-  ],
-}
+const TYPE = Object.freeze({
+  FOLDER: "FOLDER",
+  FILE: "FILE",
+})
 
-let currentFolder = root
+const nodes = [
+  { id: 1, name: "Videos", type: TYPE.FOLDER, parentId: null },
+  { id: 2, name: "Pictures", type: TYPE.FOLDER, parentId: null },
+  { id: 3, name: "Documents", type: TYPE.FOLDER, parentId: null },
+  { id: 4, name: "Music", type: TYPE.FOLDER, parentId: null },
+  { id: 5, name: "CV", type: TYPE.FOLDER, parentId: 3 },
+  { id: 6, name: "Amine Tirecht.pdf", type: TYPE.FILE, parentId: 5 },
+]
+
+let currentFolder = {
+  id: null,
+  name: "Home",
+  type: TYPE.FOLDER,
+  parentId: null,
+}
 
 main()
 
@@ -68,11 +29,14 @@ function main() {
 }
 
 function renderExplorer() {
-  explorer.innerHTML = currentFolder.children.reduce((accumulator, file) => {
-    return (
-      accumulator +
-      `<li data-id="${file.id}" class="node"><span>${file.name}</span></li>`
-    )
+  explorer.innerHTML = nodes.reduce((accumulator, node) => {
+    if (node.parentId === currentFolder.id) {
+      return (
+        accumulator +
+        `<li data-id="${node.id}" class="node"><span>${node.name}</span></li>`
+      )
+    }
+    return accumulator + ""
   }, "")
   Array.from(explorer.querySelectorAll(".node")).forEach((node) =>
     node.addEventListener("dblclick", respondToNodeDblClick)
@@ -81,6 +45,6 @@ function renderExplorer() {
 
 function respondToNodeDblClick(e) {
   const nextId = Number(e.currentTarget.dataset.id)
-  currentFolder = currentFolder.children.find((child) => child.id === nextId)
+  currentFolder = nodes.find((node) => node.id === nextId)
   renderExplorer()
 }
