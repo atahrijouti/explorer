4ef247e87e755c3b81f50f155eeb9fe245178412
Amine Tirecht <amine.tirecht@gmail.com>
2020-06-19 16:17:20 +0100

    initialize icons

--- src/explorer/helpers/node-icon.ts
+++ src/explorer/helpers/node-icon.ts
@@ -1,12 +1,24 @@
 import { NodeType } from "~app/types"
 import fileImage from "~images/file.png"
 import folderImage from "~images/folder.png"
+import fullFolderImage from "~images/full_folder.png"
+import textFileImage from "~images/text_file.png"
+import audioFileImage from "~images/audio_file.png"
 import unknownImage from "~images/unknown.png"
+import { Node } from "~app/state"
 
-export const nodeIcon = (type: NodeType) => {
-  switch (type) {
-    case NodeType.FILE: return fileImage
-    case NodeType.FOLDER: return folderImage
+export function nodeIcon(node: Node): string {
+  switch (node.type) {
+    case NodeType.FILE: return getFileImage(node)
+    case NodeType.FOLDER: return getFolderImage(node)
     default: return unknownImage
   }
+}
+
+function getFolderImage(node: Node) {
+  return folderImage
+}
+
+function getFileImage(node: Node) {
+  return fileImage
 }
