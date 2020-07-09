b91eb3953ce26566c6271b8ccfc35d2a3f5cac82
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2020-07-09 20:10:20 +0200

    push the changes to the bottom of the file for better pr review

--- src/pages/explorer/queries.ts
+++ src/pages/explorer/queries.ts
@@ -2,50 +2,6 @@ import { Node, rootFolder, state } from "~pages/explorer/state"
 import { ID, NodeType } from "~pages/explorer/types"
 import { getPathFromWindowUrl } from "~router"
 
-let nextId = 13
-
-export const dbTable: Node[] = [
-  { id: 1, name: "Videos", type: NodeType.FOLDER, parentId: null },
-  { id: 2, name: "Pictures", type: NodeType.FOLDER, parentId: null },
-  { id: 3, name: "Documents", type: NodeType.FOLDER, parentId: null },
-  { id: 4, name: "Music", type: NodeType.FOLDER, parentId: null },
-  { id: 7, name: "New folder", type: NodeType.FOLDER, parentId: null },
-  { id: 8, name: "New folder (2)", type: NodeType.FOLDER, parentId: null },
-  { id: 5, name: "CV", type: NodeType.FOLDER, parentId: 3 },
-  { id: 6, name: "Amine Tirecht.pdf", type: NodeType.FILE, parentId: 5 },
-  { id: 9, name: "Hello world.txt", type: NodeType.FILE, parentId: null },
-  { id: 10, name: "How is it going.mp3", type: NodeType.FILE, parentId: null },
-  { id: 11, name: "desktop.ini", type: NodeType.FILE, parentId: null },
-  { id: 12, name: "random.atirecht", type: NodeType.FILE, parentId: null },
-]
-
-export function findNodeFromPath(): { node: Node; breadcrumb: Node[] } | null {
-  const path = getPathFromWindowUrl()
-  if (path === "/") {
-    return {
-      breadcrumb: [rootFolder],
-      node: rootFolder,
-    }
-  }
-
-  const breadcrumb = path
-    .substr(1)
-    .split("/")
-    .map((folder) => {
-      return dbTable.find((node) => node.name === decodeURIComponent(folder))
-    })
-
-  if (!breadcrumb.every((crumb) => crumb != null)) {
-    return null
-  }
-
-  // <Node[]> is necessary otherwisse typescript thinks breadcrumb has undefined values inside
-  return {
-    breadcrumb: [rootFolder, ...(<Node[]>breadcrumb)],
-    node: <Node>breadcrumb[breadcrumb.length - 1],
-  }
-}
-
 export function findNodeById(id: ID) {
   return dbTable.find((n) => n.id === id)
 }
@@ -146,3 +102,47 @@ function getSuitableName(newName: string, nodeType: NodeType, parentId: ID) {
 
   return `${newName}${suffix ? ` (${suffix})` : ""}`
 }
+
+export function findNodeFromPath(): { node: Node; breadcrumb: Node[] } | null {
+  const path = getPathFromWindowUrl()
+  if (path === "/") {
+    return {
+      breadcrumb: [rootFolder],
+      node: rootFolder,
+    }
+  }
+
+  const breadcrumb = path
+    .substr(1)
+    .split("/")
+    .map((folder) => {
+      return dbTable.find((node) => node.name === decodeURIComponent(folder))
+    })
+
+  if (!breadcrumb.every((crumb) => crumb != null)) {
+    return null
+  }
+
+  // <Node[]> is necessary otherwisse typescript thinks breadcrumb has undefined values inside
+  return {
+    breadcrumb: [rootFolder, ...(<Node[]>breadcrumb)],
+    node: <Node>breadcrumb[breadcrumb.length - 1],
+  }
+}
+
+let nextId = 13
+
+export const dbTable: Node[] = [
+  { id: 1, name: "Videos", type: NodeType.FOLDER, parentId: null },
+  { id: 2, name: "Pictures", type: NodeType.FOLDER, parentId: null },
+  { id: 3, name: "Documents", type: NodeType.FOLDER, parentId: null },
+  { id: 4, name: "Music", type: NodeType.FOLDER, parentId: null },
+  { id: 7, name: "New folder", type: NodeType.FOLDER, parentId: null },
+  { id: 8, name: "New folder (2)", type: NodeType.FOLDER, parentId: null },
+  { id: 5, name: "CV", type: NodeType.FOLDER, parentId: 3 },
+  { id: 6, name: "Amine Tirecht.pdf", type: NodeType.FILE, parentId: 5 },
+  { id: 9, name: "Hello world.txt", type: NodeType.FILE, parentId: null },
+  { id: 10, name: "How is it going.mp3", type: NodeType.FILE, parentId: null },
+  { id: 11, name: "desktop.ini", type: NodeType.FILE, parentId: null },
+  { id: 12, name: "random.atirecht", type: NodeType.FILE, parentId: null },
+]
